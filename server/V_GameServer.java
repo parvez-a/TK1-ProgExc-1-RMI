3b1d94f0500fa7300b618402de1e668ec0fcd962
parvez-a <in.parvez@gmail.com>
2015-11-13 13:52:23 +0100

    Update V_GameServer.java

--- server/V_GameServer.java
+++ server/V_GameServer.java
@@ -16,7 +16,8 @@ import util.ClientListCellRenderer;
 
 public class V_GameServer extends JPanel{
 
-	/**
+	/**This is the View component of the Server that generates the output presentation to the user 
+	 * based on the changes in the Model component.
 	 * 
 	 */
 	private static final long serialVersionUID = 4796139289136818970L;
@@ -32,6 +33,7 @@ public class V_GameServer extends JPanel{
 	private JList<ClientInfo> listClients;
 	private DefaultListModel<ClientInfo> mdlLstClients;
 	
+	//Constructor which sets the layouts
 	public V_GameServer(){
 		layout = new BorderLayout();
 		this.setLayout(layout);
@@ -46,6 +48,7 @@ public class V_GameServer extends JPanel{
 		setVisible(true);
 	}
 	
+	//To crete left panel of Server window where list of logged in clients appears
 	private void setupWestArea(){
 		pnlWestArea = new JPanel();
 		pnlWestArea.setLayout(new BoxLayout(pnlWestArea, BoxLayout.Y_AXIS));
@@ -64,6 +67,7 @@ public class V_GameServer extends JPanel{
 		pnlWestArea.add(listClients);
 	}
 	
+	//To set up middle panel
 	private void setupMiddleArea(){
 		txtAreaStatuses = new JTextArea();
 		txtAreaStatuses.setEditable(false);
@@ -72,6 +76,7 @@ public class V_GameServer extends JPanel{
 		add(scrollingTextArea, BorderLayout.CENTER);
 	}
 	
+	//In order to show the list of logged in clients
 	public void setClients(ArrayList<ClientInfo> clients){
 		mdlLstClients.clear();
 		for (ClientInfo ci : clients){
@@ -79,6 +84,7 @@ public class V_GameServer extends JPanel{
 		}
 	}
 	
+	//To show the status message
 	public void setStatus(String stat){
 		lblStatus.setText("Status: "+stat);
 		txtAreaStatuses.append(stat+"\n");
