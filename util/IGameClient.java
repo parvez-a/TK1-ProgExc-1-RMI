ace82a878842e5709a7290651e37bff6537685fe
parvez-a <in.parvez@gmail.com>
2015-11-13 13:53:54 +0100

    Update IGameClient.java

--- util/IGameClient.java
+++ util/IGameClient.java
@@ -4,6 +4,13 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+/* By extending the interface java.rmi.Remote, IGameClient interface methods can be invoked 
+ * from another Java client. 
+ * Any object that implements this interface can be a remote object.
+ * 
+ */
+
+
 public interface IGameClient extends Remote {
 
 	public void receiveSetting(int gameWidth, int gameHeight) throws RemoteException;
